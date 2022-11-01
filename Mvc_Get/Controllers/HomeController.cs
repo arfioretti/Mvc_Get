f5f30c8acbc3def8df290b6f334022da9a694c31
aristides fioretti <arfioretti@gmail.com>
2022-11-01 18:53:01 -0300

    Add project files.

---
+++ Mvc_Get/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Get.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index1()
+        {
+            return View();
+        }
+
+        public string DataHoraAtual()
+        {
+            return DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss");
+        }
+    }
+}
